3875f2ae00649f39e2c606f05a70c41a55eda036
Avram Cosmin <avram.cosmin@vlah.io>
2024-02-19 16:02:13 +0200

    Added alternative function type check

--- src/lib/function.spec.ts
+++ src/lib/function.spec.ts
@@ -1,25 +1,51 @@
-import { OfFunctionType } from './function';
+import {OfFunctionType, OfFunctionType2} from './function';
 
 test('OfFunctionType: Expecting () => true to be true', () => {
-  expect(OfFunctionType(() => true)).toBe(true);
+    expect(OfFunctionType(() => true)).toBe(true);
 });
 
 test('OfFunctionType: Expecting function tes(): void {} to be true', () => {
-  expect(OfFunctionType(function tes(): void {})).toBe(true);
+    expect(OfFunctionType(function tes(): void {
+    })).toBe(true);
 });
 
 test('OfFunctionType: Expecting String to be true', () => {
-  expect(OfFunctionType(String)).toBe(true);
+    expect(OfFunctionType(String)).toBe(true);
 });
 
 test('OfFunctionType: Expecting 2 to be false', () => {
-  expect(OfFunctionType('2')).toBe(false);
+    expect(OfFunctionType('2')).toBe(false);
 });
 
 test('OfFunctionType: Expecting true to be false', () => {
-  expect(OfFunctionType(true)).toBe(false);
+    expect(OfFunctionType(true)).toBe(false);
 });
 
 test('OfFunctionType: Expecting {} to be false', () => {
-  expect(OfFunctionType({})).toBe(false);
+    expect(OfFunctionType({})).toBe(false);
+});
+
+test('OfFunctionType2: Expecting () => true to be true', () => {
+    expect(OfFunctionType2(() => true)).toBe(true);
+});
+
+test('OfFunctionType2: Expecting function tes(): void {} to be true', () => {
+    expect(OfFunctionType2(function tes(): void {
+    })).toBe(true);
+});
+
+test('OfFunctionType2: Expecting String to be false', () => {
+    expect(OfFunctionType2(String)).toBe(true);
+});
+
+test('OfFunctionType2: Expecting 2 to be false', () => {
+    expect(OfFunctionType2('2')).toBe(false);
+});
+
+test('OfFunctionType2: Expecting true to be false', () => {
+    expect(OfFunctionType2(true)).toBe(false);
+});
+
+test('OfFunctionType2: Expecting {} to be false', () => {
+    expect(OfFunctionType2({})).toBe(false);
 });
--- src/lib/function.ts
+++ src/lib/function.ts
@@ -1,11 +1,25 @@
 import { OfType } from './of-type';
 
 /**
- * Checks if the given argument is of type T.
+ * This function checks if the given argument is of a specific type T.
+ * It uses the OfType function to perform the type check.
  *
- * @param {unknown} arg - The argument to be checked.
- * @return {boolean} - True if the argument is of type T, false otherwise.
+ * @template T The expected type of the argument.
+ * @param {unknown} arg - The argument whose type is to be checked.
+ * @returns {boolean} - Returns true if the argument is of type T, false otherwise.
  */
 export function OfFunctionType<T>(arg: unknown): arg is T {
   return OfType(arg, 'Function');
+}
+
+/**
+ * This function checks if the given argument is a function.
+ * It uses the typeof operator to perform the type check.
+ *
+ * @template T The expected type of the argument.
+ * @param {unknown} arg - The argument whose type is to be checked.
+ * @returns {boolean} - Returns true if the argument is a function, false otherwise.
+ */
+export function OfFunctionType2<T>(arg: unknown): arg is T {
+  return typeof arg === 'function';
 }
